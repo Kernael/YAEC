fbe5506061f2b612cc2b5994a10082679807fe23
Eliot BARDET <eliot.bardet@epitech.eu>
2015-12-06 15:39:35 +0100

    handles new_msg event

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -4,8 +4,21 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 
 socket.connect()
 
-// Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("rooms:lobby", {})
+let channel   = socket.channel("rooms:lobby", {})
+let chatInput = $("#chat-input")
+let messages  = $("#messages")
+
+chatInput.on("keypress", event => {
+    if (event.keyCode === 13) {
+        channel.push("new_msg", {body: chatInput.val()})
+        chatInput.val("")
+    }
+})
+
+channel.on("new_msg", payload => {
+    messages.append(`<br/>[${Date()}] ${payload.body}`)
+})
+
 channel.join()
   .receive("ok", resp => { console.log("Joined successfully", resp) })
   .receive("error", resp => { console.log("Unable to join", resp) })
